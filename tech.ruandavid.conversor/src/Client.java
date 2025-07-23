0362c2b1ee0272ba5b36a1464b4b4fa30c094c2c
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-07-23 17:50:07 -0300

    tidy client implementation

--- tech.ruandavid.conversor/src/Client.java
+++ tech.ruandavid.conversor/src/Client.java
@@ -7,32 +7,37 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class Client {
-    private String url;
+    private final HttpClient client;
+    private final HttpRequest request;
 
-    private HttpClient client = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_1_1)
-            .followRedirects(HttpClient.Redirect.NORMAL)
-            .connectTimeout(Duration.ofSeconds(20))
-            .build();
     // private HttpResponse<String> response;
     // Abaixo vou aproveitar o exemplo assincrono do javadocs para a classe http
-    private HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .timeout(Duration.ofMinutes(2))
-            .header("Content-Type", "application/json")
-            .build();
 
     public Client(String url) {
-        this.url = url;
+        this.client = HttpClient.newBuilder().build();
+
+        try {
+            this.request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .timeout(Duration.ofMinutes(2))
+                    .header("Content-Type", "application/json")
+                    .build();
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Url invalida: " +url, e);
+        }
+    }
+
+    public String sendRequest(){
+        return client.sendAsync(this.request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .join();
     }
 
     public static void main(String[] args) {
-        //Client client = new Client("https://viacep.com.br/ws/01001000/json");
-        Client client = new Client("https://viacep.com.br/ws/01001000/json");
+        Client client = new Client("https://viacep.com.br/ws/01001000/json/");
 
-        client.client.sendAsync(client.request, HttpResponse.BodyHandlers.ofString())
-        .thenApply(HttpResponse::body)
-        .thenAccept(System.out::println)
-                .join();
+        String output = client.sendRequest();
+
+        System.out.println(output);
     }
 }
