ef1230f830cd61ac52d56aa4576e647ff0565409
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-08-15 10:20:24 -0300

    refactor print currencies method

--- tech.ruandavid.conversor/src/Moedas.java
+++ tech.ruandavid.conversor/src/Moedas.java
@@ -1,5 +1,9 @@
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 
 public class Moedas {
@@ -8,5 +12,22 @@ public class Moedas {
 
     public List<List<String>> getMoedasSuportadas() {
         return moedasSuportadas;
+    }
+
+    public void printMoedas(){
+        Gson gson = new Gson();
+
+        try {
+            JsonReader jr = new JsonReader(new FileReader("moedas.json"));
+            Moedas moedasSuportadas = gson.fromJson(jr, Moedas.class);
+
+            moedasSuportadas.getMoedasSuportadas()
+                    .stream()
+                    .forEach( moeda -> System.out.println(moeda.get(1) + "\nCódigo: " + moeda.get(0)
+                            )
+                    );
+        } catch (FileNotFoundException e) {
+            System.out.println("Erro ao encontrar arquivo Json, já rodou a classe Persistor? stacktrace:" + e);
+        }
     }
 }
